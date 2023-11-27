b0653c2fe1d9f23e96120d33de9aab2a4cb72841
JacobChurch2 <jchurch@student.neumont.edu>
2023-11-27 14:05:36 -0700

    Add project files.

---
+++ Source/RayTracer/Main.cpp
@@ -0,0 +1,34 @@
+#include "Renderer.h"
+#include <iostream>
+#include <SDL.h>
+
+using namespace std;
+
+int main(int, char**)
+{
+	std::cout << "hello world!\n";
+
+		//<create renderer>
+	Renderer renderer;
+		//<initialize renderer>
+	renderer.Initialize();
+		//<create renderer window with size of 400, 300>
+	renderer.CreateWindow("The Window", 400, 300);
+		bool quit = false;
+	while (!quit)
+	{
+		SDL_Event event;
+		SDL_PollEvent(&event);
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			quit = true;
+			break;
+		}
+	}
+
+		//<shutdown renderer>
+	renderer.Shutdown();
+
+	return 0;
+}
